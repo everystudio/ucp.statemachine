94abd08cb9d76d4c98031427bc41debcf4de8f2d
watanabe@marida <watanabe.everystudio@gmail.com>
2023-03-29 11:29:39 +0900

    namespace変更 一部メソッド追加

--- Scripts/StateBase.cs
+++ Scripts/StateBase.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StateBase<T> where T : StateMachineBase<T>
+namespace anogame
 {
-	protected T machine;
-	public StateBase(T _machine)
-	{
-		machine = _machine;
-	}
+    public abstract class StateBase<T> where T : StateMachineBase<T>
+    {
+        protected T machine;
+        public StateBase(T machine)
+        {
+            this.machine = machine;
+        }
 
-	public virtual void OnEnterState()
-	{
-	}
-	public virtual void OnUpdateState()
-	{
-	}
-	public virtual void OnFixedUpdateState()
-	{
-	}
-	public virtual void OnExitState()
-	{
-	}
+        public virtual void OnEnterState()
+        {
+        }
+        public virtual void OnUpdateState()
+        {
+        }
+        public virtual void OnFixedUpdateState()
+        {
+        }
+        public virtual void OnExitState()
+        {
+        }
+        public void ChangeState(StateBase<T> state)
+        {
+            machine.ChangeState(state);
+        }
+    }
 }
--- Scripts/StateMachineBase.cs
+++ Scripts/StateMachineBase.cs
@@ -3,46 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public abstract class StateMachineBase<T> : MonoBehaviour where T : StateMachineBase<T>
+namespace anogame
 {
-	protected StateBase<T> stateCurrent;
+    public abstract class StateMachineBase<T> : MonoBehaviour where T : StateMachineBase<T>
+    {
+        protected StateBase<T> stateCurrent;
 
-	public UnityEvent OnBegin = new UnityEvent();
-	public UnityEvent OnEnd = new UnityEvent();
+        public UnityEvent OnBegin = new UnityEvent();
+        public UnityEvent OnEnd = new UnityEvent();
 
-	protected virtual void OnUpdatePrev() { }
-	protected virtual void OnUpdateAfter() { }
+        protected virtual void OnUpdatePrev() { }
+        protected virtual void OnUpdateAfter() { }
 
-	protected virtual void OnFixedUpdatePrev() { }
-	protected virtual void OnFixedUpdateAfter() { }
+        protected virtual void OnFixedUpdatePrev() { }
+        protected virtual void OnFixedUpdateAfter() { }
 
-	public void SetState(StateBase<T> _state)
-	{
-		if (stateCurrent != null)
-		{
-			stateCurrent.OnExitState();
-		}
-		stateCurrent = _state;
-		stateCurrent.OnEnterState();
-	}
-	private void Update()
-	{
-		OnUpdatePrev();
-		if (stateCurrent != null)
-		{
-			stateCurrent.OnUpdateState();
-		}
-		OnUpdateAfter();
-	}
+        public void ChangeState(StateBase<T> state)
+        {
+            if (stateCurrent != null)
+            {
+                stateCurrent.OnExitState();
+            }
+            stateCurrent = state;
+            stateCurrent.OnEnterState();
+        }
+        private void Update()
+        {
+            OnUpdatePrev();
+            if (stateCurrent != null)
+            {
+                stateCurrent.OnUpdateState();
+            }
+            OnUpdateAfter();
+        }
 
-	private void FixedUpdate()
-	{
-		OnFixedUpdatePrev();
-		if (stateCurrent != null)
-		{
-			stateCurrent.OnFixedUpdateState();
-		}
-		OnFixedUpdateAfter();
-	}
-
+        private void FixedUpdate()
+        {
+            OnFixedUpdatePrev();
+            if (stateCurrent != null)
+            {
+                stateCurrent.OnFixedUpdateState();
+            }
+            OnFixedUpdateAfter();
+        }
+    }
 }
